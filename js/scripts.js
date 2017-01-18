2f26164b41bc57249b50d7b6d13a38796a98d2a2
Felix Oporto Lopez and Koji Nakagawa <felix@>
2017-01-18 09:31:43 -0800

    add user interface logic, it will take the input value from the form and pass as an argument to the function

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,22 @@ $(function(){
   // If the year can be evenly divided by 100, it is NOT a leap year, unless;
   // The year is also evenly divisible by 400. Then it is a leap year.
 
-  var theyear= 1600;
 
-  if ( (theyear % 4 === 0 && theyear % 100 != 0) ||  theyear % 400 === 0){
-    alert("Leap Year!");
+//Front-End
+  $('form#leap-form').submit(function(event){
+    event.preventDefault();
+    var inputValue = parseInt($('input#userInput').val());
+    leapChecker(inputValue);
+  });
 
-  } else {
-    alert("Not Leap Year");
-  }
 
+
+//Back-End Logic
+  var leapChecker = function(inputValue){
+    if ( (inputValue % 4 === 0 && inputValue % 100 != 0) ||  inputValue % 400 === 0){
+      alert("Leap Year!");
+    } else {
+      alert("Not Leap Year");
+    };
+  };
 });
